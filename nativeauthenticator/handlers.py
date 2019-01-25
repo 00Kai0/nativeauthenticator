6b97563a4446a4f987312cb5d8a6e3e1f114281e
Leticia Portella <leportella@gmail.com>
2019-01-25 10:48:30 +0000

    Add input of email when config is set to True

--- nativeauthenticator/handlers.py
+++ nativeauthenticator/handlers.py
@@ -28,7 +28,10 @@ class SignUpHandler(LocalBase):
     """Render the sign in page."""
     async def get(self):
         self._register_template_path()
-        html = self.render_template('signup.html')
+        html = self.render_template(
+            'signup.html',
+            ask_email=self.authenticator.ask_email_on_signup,
+        )
         self.finish(html)
 
     def get_result_message(self, message_type):
@@ -60,6 +63,7 @@ class SignUpHandler(LocalBase):
 
         html = self.render_template(
             'signup.html',
+            ask_email=self.authenticator.ask_email_on_signup,
             result_message=result_message,
             alert=alert
         )
